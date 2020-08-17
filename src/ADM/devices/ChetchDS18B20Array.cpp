40418bb6e4037b72d33ff175ec4eebc2a676bfa4
billchetch <45006134+billchetch@users.noreply.github.com>
2020-08-18 02:47:32 +0800

    gg

--- src/ADM/devices/ChetchDS18B20Array.cpp
+++ src/ADM/devices/ChetchDS18B20Array.cpp
@@ -6,10 +6,51 @@ namespace Chetch{
 		//empty
 	}
 
+	DS18B20Array::~DS18B20Array(){
+		if (tempSensors != NULL)delete tempSensors;
+		if (oneWire != NULL)delete oneWire;
+	}
 
 	void DS18B20Array::configure(bool initial, ADMMessage *message, ADMMessage *response) {
 		ArduinoDevice::configure(initial, message, response);
 
-		response->addValue("Yep", "Cool", false);
+		if (initial) {
+			if (tempSensors != NULL)delete tempSensors;
+			if (oneWire != NULL)delete oneWire;
+		}
+
+		if (oneWire == NULL) {
+			int owPin = message->argumentAsInt(3);
+			oneWire = new OneWire(owPin);
+			tempSensors = new DallasTemperature(oneWire);
+			tempSensors->begin();
+			numberOfTempSensors = tempSensors->getDeviceCount();
+			response->addInt("SensorCount", numberOfTempSensors);
+			response->addInt("OneWirePin", owPin);
+		}
+	}
+
+	bool DS18B20Array::handleCommand(ADMMessage *message, ADMMessage *response) {
+		switch (message->command) {
+			case ADMMessage::COMMAND_TYPE_READ:
+				if (numberOfTempSensors > 0) {
+					tempSensors->requestTemperatures();
+					DeviceAddress tempDeviceAddress;
+					response->type = ADMMessage::TYPE_DATA;
+					response->target = target;
+					response->addInt("SensorCount", numberOfTempSensors);
+					char tempKey[16];
+					for (int i = 0; i < numberOfTempSensors; i++) {
+						tempSensors->getAddress(tempDeviceAddress, i);
+						float celsius = tempSensors->getTempC(tempDeviceAddress);
+						sprintf(tempKey, "Temperature-%d", i);
+						response->addFloat(tempKey, celsius);
+					}
+				}
+				return true;
+
+			default:
+				return false;
+		}
 	}
 } //end namespace
--- src/ADM/devices/ChetchDS18B20Array.h
+++ src/ADM/devices/ChetchDS18B20Array.h
@@ -1,12 +1,20 @@
 #include <Arduino.h>
 #include "../ChetchArduinoDevice.h"
 #include "../ChetchADMMessage.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 namespace Chetch{
   class DS18B20Array : public ArduinoDevice {
+	private:
+		OneWire *oneWire = NULL;
+		DallasTemperature *tempSensors = NULL;
+		int numberOfTempSensors = 0;
+
     public:
 		DS18B20Array(byte tgt, byte cat, char *did, char *dn);
-
+		~DS18B20Array();
 		void configure(bool initial, ADMMessage *message, ADMMessage *response);
+		bool handleCommand(ADMMessage *message, ADMMessage *response);
   };
 } //end namespace	
