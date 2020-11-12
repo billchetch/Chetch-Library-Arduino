7cd05c1116d44565f6d56a4b463658c21344dc16
billchetch <45006134+billchetch@users.noreply.github.com>
2020-11-13 00:49:19 +0800

    d

--- src/ADM/devices/ChetchDS18B20Array.cpp
+++ src/ADM/devices/ChetchDS18B20Array.cpp
@@ -15,6 +15,7 @@ namespace Chetch{
 				delete[] deviceAddresses[i];
 			}
 			delete[] deviceAddresses;
+			delete[] temperatures;
 		}
 	}
 
@@ -26,22 +27,30 @@ namespace Chetch{
 			oneWire = new OneWire(owPin);
 			tempSensors = new DallasTemperature(oneWire);
 			tempSensors->begin();
-			tempSensors->setResolution(9); //0.5 degrees celsius .... faster look up times than 11 or 12 bit
+			tempSensors->setResolution(message->argumentAsInt(MSG_DEVICE_PARAMS_START_INDEX + 1)); //set the resolution
 
 			//get number of sensors and their addresses (this is to speed up the handlecommand loop)
 			numberOfTempSensors = tempSensors->getDeviceCount();
-			deviceAddresses = new uint8_t*[numberOfTempSensors];
-			DeviceAddress tempDeviceAddress;
-			for (int i = 0; i < numberOfTempSensors; i++) {
-				tempSensors->getAddress(tempDeviceAddress, i);
-				deviceAddresses[i] = new uint8_t[8];
-				for(int j = 0; j < 8; j++){
-					deviceAddresses[i][j] = tempDeviceAddress[j];
+
+			if(numberOfTempSensors > 0){
+				deviceAddresses = new uint8_t*[numberOfTempSensors];
+				temperatures = new float[numberOfTempSensors];
+
+				DeviceAddress tempDeviceAddress;
+				for (int i = 0; i < numberOfTempSensors; i++) {
+					tempSensors->getAddress(tempDeviceAddress, i);
+					deviceAddresses[i] = new uint8_t[8];
+					for(int j = 0; j < 8; j++){
+						deviceAddresses[i][j] = tempDeviceAddress[j];
+					}
+					temperatures[i] = 0.0f;
 				}
 			}
-
 			response->addInt(numberOfTempSensors);
 			response->addInt(owPin);
+
+			readInterval = readInterval / numberOfTempSensors;
+			lastRead = millis();
 		}
 	}
 
@@ -49,14 +58,11 @@ namespace Chetch{
 		switch (message->commandType()) {
 			case ADMMessage::COMMAND_TYPE_READ:
 				if (numberOfTempSensors > 0) {
-					tempSensors->requestTemperatures();
 					response->type = (byte)ADMMessage::TYPE_DATA;
 					response->target = target;
 					response->addInt(numberOfTempSensors);
-					
-					for (int i = 0; i < numberOfTempSensors; i++) {
-						float celsius = tempSensors->getTempC(deviceAddresses[i]);
-						response->addFloat(celsius);
+					for(int i = 0; i < numberOfTempSensors; i++){
+						response->addFloat(temperatures[i]);
 					}
 				}
 				return true;
@@ -64,5 +70,21 @@ namespace Chetch{
 			default:
 				return false;
 		}
+	}
+
+	ADMMessage* DS18B20Array::loop(){
+		if(numberOfTempSensors <= 0 || lastRead <= 0 || readInterval < 1000)return NULL; //we are not ready to read yet
+
+		if(millis() - lastRead > readInterval){
+			tempSensors->requestTemperaturesByAddress(deviceAddresses[currentIndex]);
+
+			float celsius = tempSensors->getTempC(deviceAddresses[currentIndex]);
+			temperatures[currentIndex] = celsius;
+			lastRead = millis();
+			currentIndex++;
+			if(currentIndex == numberOfTempSensors)currentIndex = 0;
+		}
+		//
+		return NULL;
 	}
 } //end namespace
--- src/ADM/devices/ChetchDS18B20Array.h
+++ src/ADM/devices/ChetchDS18B20Array.h
@@ -11,11 +11,16 @@ namespace Chetch{
 		DallasTemperature *tempSensors = NULL;
 		int numberOfTempSensors = 0;
 		uint8_t **deviceAddresses = NULL;
+		int readInterval = 20000;
+		float *temperatures;
+		byte currentIndex = 0;
+		unsigned long lastRead = 0;
 
     public:
 		DS18B20Array(byte tgt, byte cat, char *dn);
 		~DS18B20Array();
 		void configure(bool initial, ADMMessage *message, ADMMessage *response);
 		bool handleCommand(ADMMessage *message, ADMMessage *response);
+		ADMMessage* loop();
   };
 } //end namespace	
